c3fc534281b2b7dcee5a737bd92088162839db7f
Fly Dev <dev@fly.io>
2026-07-05 20:56:48 +0000

    update test config for postgresql

--- config/test.exs
+++ config/test.exs
@@ -6,12 +6,12 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :captablex, Captablex.Repo,
-  database: Path.expand("../captablex_test.db", __DIR__),
-  pool_size: 5,
-  pool: Ecto.Adapters.SQL.Sandbox
-
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "captablex_test#{System.get_env(\"MIX_TEST_PARTITION\")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: System.schedulers_online() * 2
 config :captablex, CaptablexWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
   secret_key_base: "ZwXIkVOFUPBSojg8gb6ldwCz+Qh8mZFdBlUNCArW/NBPozdwpDhnuCB95EneBZNh",
