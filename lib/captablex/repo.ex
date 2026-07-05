250ff892c463ceb651c0b9e7b01fed3a50a77f8e
Fly Dev <dev@fly.io>
2026-07-05 21:04:15 +0000

    fix repo syntax error

--- lib/captablex/repo.ex
+++ lib/captablex/repo.ex
@@ -1,7 +1,10 @@
 defmodule Captablex.Repo do
   use Ecto.Repo,
     otp_app: :captablex,
-  use Ecto.Repo,
-    otp_app: :captablex,
     adapter: Ecto.Adapters.Postgres
+
+  @doc """
+  A small callback to handle dynamic repos in tests
+  """
+  def init(_type, config), do: {:ok, config}
 end
