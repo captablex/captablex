f0343323a159be72e8c04d856887ad63e47641bd
Fly Dev <dev@fly.io>
2026-07-05 21:35:09 +0000

    fix test config syntax error

--- config/test.exs
+++ config/test.exs
@@ -9,22 +9,25 @@ config :captablex, Captablex.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "captablex_test#{System.get_env(\"MIX_TEST_PARTITION\")}",
+  database: "captablex_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
 config :captablex, CaptablexWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
-  secret_key_base: "ZwXIkVOFUPBSojg8gb6ldwCz+Qh8mZFdBlUNCArW/NBPozdwpDhnuCB95EneBZNh",
+  secret_key_base: "AXqWH2YjjHOXg6PcQ4eLBJvRUBxxuMFdDEfMPLNNrgNfG+n0MpU3fgWL14kwJwuH",
   server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
 
 # In test we don't send emails
 config :captablex, Captablex.Mailer, adapter: Swoosh.Adapters.Test
 
 # Disable swoosh api client as it is only required for production adapters
 config :swoosh, :api_client, false
-
-# Print only warnings and errors during test
-config :logger, level: :warning
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
