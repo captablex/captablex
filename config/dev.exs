801a936c7f684aa879c9acff513d7dfb3735259a
Fly Dev <dev@fly.io>
2026-07-05 20:56:39 +0000

    update dev config for postgresql

--- config/dev.exs
+++ config/dev.exs
@@ -2,17 +2,14 @@ import Config
 
 # Configure your database
 config :captablex, Captablex.Repo,
-  database: Path.expand("../captablex_dev.db", __DIR__),
-  pool_size: 5,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "captablex_dev",
   stacktrace: true,
-  show_sensitive_data_on_connection_error: true
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
 
-# For development, we disable any cache and enable
-# debugging and code reloading.
-#
-# The watchers configuration can be used to run external
-# watchers to your application. For example, we can use it
-# to bundle .js and .css sources.
 config :captablex, CaptablexWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
