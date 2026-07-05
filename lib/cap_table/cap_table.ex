779c468a79e8223a2df6d848d41a0d9393fec0af
Fly Dev <dev@fly.io>
2026-07-05 16:35:13 +0000

    add CapTable context functions

--- lib/cap_table/cap_table.ex
+++ lib/cap_table/cap_table.ex
@@ -7,6 +7,9 @@ defmodule CapTable do
   if it comes from the database, an external API or others.
   """
 
+  import Ecto.Query
+  alias CapTable.Repo
+
   defmodule Stakeholder do
     use Ecto.Schema
     import Ecto.Changeset
@@ -115,5 +118,156 @@ defmodule CapTable do
         "exercise"
       ])
     end
+  end
+
+  # Stakeholder functions
+  def list_stakeholders do
+    Repo.all(Stakeholder)
+  end
+
+  def get_stakeholder!(id), do: Repo.get!(Stakeholder, id)
+
+  def create_stakeholder(attrs \\ %{}) do
+    %Stakeholder{}
+    |> Stakeholder.changeset(attrs)
+    |> Repo.insert()
+    |> case do
+      {:ok, stakeholder} ->
+        Phoenix.PubSub.broadcast(
+          CapTable.PubSub,
+          "cap_table:updates",
+          {:stakeholder_created, stakeholder}
+        )
+
+        {:ok, stakeholder}
+
+      error ->
+        error
+    end
+  end
+
+  # Stock Class functions
+  def list_stock_classes do
+    Repo.all(StockClass)
+  end
+
+  def get_stock_class!(id), do: Repo.get!(StockClass, id)
+
+  def create_stock_class(attrs \\ %{}) do
+    %StockClass{}
+    |> StockClass.changeset(attrs)
+    |> Repo.insert()
+    |> case do
+      {:ok, stock_class} ->
+        Phoenix.PubSub.broadcast(
+          CapTable.PubSub,
+          "cap_table:updates",
+          {:stock_class_created, stock_class}
+        )
+
+        {:ok, stock_class}
+
+      error ->
+        error
+    end
+  end
+
+  # Security Issuance functions
+  def list_securities do
+    Repo.all(SecurityIssuance)
+    |> Repo.preload([:stakeholder, :stock_class])
+  end
+
+  def get_security!(id) do
+    Repo.get!(SecurityIssuance, id)
+    |> Repo.preload([:stakeholder, :stock_class])
+  end
+
+  def issue_security(attrs \\ %{}) do
+    %SecurityIssuance{}
+    |> SecurityIssuance.changeset(attrs)
+    |> Repo.insert()
+    |> case do
+      {:ok, security} ->
+        security = Repo.preload(security, [:stakeholder, :stock_class])
+
+        Phoenix.PubSub.broadcast(
+          CapTable.PubSub,
+          "cap_table:updates",
+          {:security_issued, security}
+        )
+
+        {:ok, security}
+
+      error ->
+        error
+    end
+  end
+
+  # Transaction functions
+  def list_transactions do
+    from(t in Transaction,
+      order_by: [desc: t.transaction_date],
+      preload: [:stakeholder, :security]
+    )
+    |> Repo.all()
+  end
+
+  def create_transaction(attrs \\ %{}) do
+    %Transaction{}
+    |> Transaction.changeset(attrs)
+    |> Repo.insert()
+    |> case do
+      {:ok, transaction} ->
+        transaction = Repo.preload(transaction, [:stakeholder, :security])
+
+        Phoenix.PubSub.broadcast(
+          CapTable.PubSub,
+          "cap_table:updates",
+          {:transaction_created, transaction}
+        )
+
+        {:ok, transaction}
+
+      error ->
+        error
+    end
+  end
+
+  # Ownership calculation
+  def calculate_ownership_breakdown do
+    securities = list_securities()
+    total_shares = Enum.reduce(securities, 0, fn sec, acc -> acc + sec.shares end)
+
+    securities
+    |> Enum.group_by(& &1.stakeholder_id)
+    |> Enum.map(fn {stakeholder_id, secs} ->
+      shares = Enum.reduce(secs, 0, fn sec, acc -> acc + sec.shares end)
+
+      percentage =
+        if total_shares > 0, do: (shares / total_shares * 100) |> Float.round(2), else: 0.0
+
+      stakeholder = Enum.at(secs, 0).stakeholder
+
+      {stakeholder_id,
+       %{
+         stakeholder: stakeholder,
+         shares: shares,
+         percentage: percentage
+       }}
+    end)
+    |> Map.new()
+  end
+
+  def get_total_shares_outstanding do
+    from(s in SecurityIssuance, select: sum(s.shares))
+    |> Repo.one()
+    |> Kernel.||(0)
+  end
+
+  def get_total_shares_authorized do
+    from(sc in StockClass, select: sum(sc.shares_authorized))
+    |> Repo.one()
+    |> Kernel.||(0)
   end
 end
